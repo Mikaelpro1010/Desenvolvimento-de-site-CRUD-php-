fc2a3343fb0b195c74b9a1fa31d58d2a49904cd5
Mikael Aguiar <93383052+Mikaelpro1010@users.noreply.github.com>
2022-05-09 20:34:56 -0300

    Update visualizar.php

--- visualizar.php
+++ visualizar.php
@@ -4,7 +4,7 @@ include_once "conexao.php";
 $id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
 
 if (!empty($id)){
-    $query_usuario = "SELECT id, nome, email FROM usuarios WHERE id =:id LIMIT 1";
+    $query_usuario = "SELECT id, nome, email, sexo, idade FROM usuarios WHERE id =:id LIMIT 1";
     $result_usuario = $conn->prepare($query_usuario);
     $result_usuario->bindParam(':id', $id);
     $result_usuario->execute();
