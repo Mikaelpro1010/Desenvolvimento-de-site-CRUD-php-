b1020c4e08504856de6f51df69833a3e2f29ebe5
Mikael Aguiar <93383052+Mikaelpro1010@users.noreply.github.com>
2022-05-09 08:27:51 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Document</title>
 </head>
-
 <body>
     <div class="container">
         <div class="row mt-4">
@@ -22,10 +21,8 @@
             
             <span id="msgAlerta"></span>
             <span class="listar-usuarios"></span>
-
-
+            
         </div>
-
         <div class="modal fade" id="cadUsuarioModal" tabindex="-1" aria-labelledby="#cadUsuarioModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
@@ -54,8 +51,30 @@
             </div>
         </div>
 
+        <div class="modal fade" id="visUsuarioModal" tabindex="-1" aria-labelledby="#cadUsuarioModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="visUsuarioModalLabel">Detalhes do usuário</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                            <span id="msgAlertaErroVis"></span>
+                            <dl class="row">
+                                <dt class="col-sm-3">ID</dt>
+                                <dd class="col-sm-9"><span id="idUsuario"></span></dd>
+
+                                <dt class="col-sm-3">Nome</dt>
+                                <dd class="col-sm-9"><span id="nomeUsuario"></span></dd>
+
+                                <dt class="col-sm-3">E-mail</dt>
+                                <dd class="col-sm-9"><span id="emailUsuario"></span></dd>
+                            </dl>
+                    </div>
+                </div>
+            </div>
+        </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
         <script src="js/custom.js"></script>
 </body>
-
 </html>
